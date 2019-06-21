7b8d37ef4c4ec5e8002d160d2253956abd22bcbc
GRZ <908482128@qq.com>
2019-06-21 22:01:23 +0800

    添加模块Home&Login，配置路由导航

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-
+  routes: [{
+    name: 'home',
+    // 路由的名字和组件名没关系，是path的别名
+    // 跳转的时候可以直接$router.push({name:'xxx'})
+    // 给路由起名字是好的做法
+    // @表示src目录，无论当前文件在哪里，@都是src
+    path: '/',
+    component: () => import('@/views/home')
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/login')
+  }
   ]
 })
