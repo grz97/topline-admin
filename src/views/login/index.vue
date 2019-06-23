250de4f74aaa392602b3dfaf43a52a9555b115bc
GRZ <908482128@qq.com>
2019-06-23 20:29:47 +0800

    用户登录-验证同意用户协议字段

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,12 +5,12 @@
         <img src="./logo_index.png" alt="黑马头条">
       </div>
       <div class="login-form">
-      <!--
+        <!--
           表单验证：
           rules: 配置验证规则
           将需要验证的字段通过 prop 属性配置到el-form-item组件上
           ref: 获取表单组件 可以手动调用表单组件的验证方法
-      -->
+        -->
         <el-form ref="ruleForm" :model="form" :rules="rules">
           <el-form-item prop="mobile">
             <el-input v-model="form.mobile" placeholder="手机号"></el-input>
@@ -25,14 +25,22 @@
               <!-- <el-button v-show="!show" class="count">{{count}}s</el-button> -->
             </el-col>
           </el-form-item>
+          <el-form-item prop="agree">
+            <el-checkbox v-model="form.agree"></el-checkbox>
+            <span>
+              我已阅读并同意
+              <a href="#">用户协议</a>和
+              <a href="#">隐私条款</a>
+            </span>
+          </el-form-item>
           <el-form-item>
             <!--给组件加class 会作用到它的根元素-->
             <el-button
-             class="btn-login"
-             type="primary"
-             @click="handLogin"
-             :loading="loginLoading"
-             >登录</el-button>
+              class="btn-login"
+              type="primary"
+              @click="handLogin"
+              :loading="loginLoading"
+            >登录</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -40,131 +48,154 @@
   </div>
 </template>
 <script>
-import axios from 'axios'
-import '@/vendor/gt' // git.js会向全局window 暴露一个函数 initGeetest
+import axios from "axios";
+import "@/vendor/gt"; // git.js会向全局window 暴露一个函数 initGeetest
 export default {
-  name: 'AppLogin',
+  name: "AppLogin",
   data() {
     return {
-    //   show:'true',
-    //   count:'',
-    //   timer:null,
-      form: { // 表单数据
-        mobile: '18734800169',
-        code: ''
+      //   show:'true',
+      //   count:'',
+      //   timer:null,
+      form: {
+        // 表单数据
+        mobile: "18734800169",
+        code: "",
+        agree: "" // 是否同意用户协议
       },
       loginLoading: false, // 登录按钮的loading 状态
-      rules: { // 表单验证规则
+      rules: {
+        // 表单验证规则
         mobile: [
-            { required: true, message: '请输入手机号', trigger: 'blur' },
-            { min: 11, max: 11, message: '长度为11个字符', trigger: 'blur' }
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          { min: 11, max: 11, message: "长度为11个字符", trigger: "blur" }
         ],
         code: [
-           { required: true, message: '请输入验证码', trigger: 'blur' },
-           { min: 6, max: 6, message: '长度为6个字符', trigger: 'blur' }
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          { min: 6, max: 6, message: "长度为6个字符", trigger: "blur" }
+        ],
+        agree: [
+          { required: true, message: "请同意用户协议", trigger: "change" },
+          // pattern 正则表达式
+          { pattern: /true/, message: "请同意用户协议", trigger: "change" }
         ]
       },
-       captchaObj:null // 通过initGeetest得到的级验验证码对象
-      }
-   },
+      captchaObj: null // 通过initGeetest得到的级验验证码对象
+    };
+  },
   methods: {
     handLogin() {
-        //  表单组件有一个方法 validatae 可以用来获取当前表单的验证状态
-        this.$refs['ruleForm'].validate(valid => {
+      //  表单组件有一个方法 validatae 可以用来获取当前表单的验证状态
+      this.$refs["ruleForm"].validate(valid => {
         if (!valid) {
-          return
+          return;
         }
-        this.submitLogin()
-      })
+        this.submitLogin();
+      });
     },
     submitLogin() {
-        this.loginLoading = true
-        axios({
-        method:'POST',
-        url:'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
-        data:this.form
-      }).then(res => {
-        // Element 提供的提示文本信息组件 这也是组件使用的一种方式  大于等于200或者小于400的状态码都会进入这里
-        this.$message({
-          message: '登录成功',
-          type: 'success'
-        })
-        this.loginLoading = false
-        // 跳转到路由首页 建议使用name跳转 路由传参非常方便
-        this.$router.push({
-          name: 'home'
-        })
+      // 登录
+      this.loginLoading = true; // 登录加载
+      axios({
+        method: "POST",
+        url: "http://ttapi.research.itcast.cn/mp/v1_0/authorizations",
+        data: this.form // form表单
       })
-      .catch(err => {
-        // console.log(err)
-        if (err.response.status === 400) {
-           this.$message.error('登录失败 手机号或验证码错误') // 小于等于400的http状态码都会进入这里
-        }
-        this.loginLoading = false
-      })
+        .then(res => {
+          // Element 提供的提示文本信息组件 这也是组件使用的一种方式  大于等于200或者小于400的状态码都会进入这里
+          this.$message({
+            message: "登录成功",
+            type: "success"
+          });
+          this.loginLoading = false;
+          // 跳转到路由首页 建议使用name跳转 路由传参非常方便
+          this.$router.push({
+            name: "home"
+          });
+        })
+        .catch(err => {
+          // console.log(err)
+          if (err.response.status === 400) {
+            this.$message.error("登录失败 手机号或验证码错误"); // 小于等于400的http状态码都会进入这里
+          }
+          this.loginLoading = false;
+        });
     },
     handleSendCode() {
-        const { mobile } = this.form
-        // 如果已经初始化了，就直接verify
-        if (this.captchaObj) {
-            //
-          return this.captchaObj.verify()
+      this.$refs["ruleForm"].validateField("mobile", errorMessage => {
+        if (errorMessage.trim().length > 0) {
+          return;
         }
+        this.showGeetest();
+      });
+    },
+    showGeetest() {
+      const { mobile } = this.form;
+      // 如果已经初始化了，就直接verify
+      if (this.captchaObj) {
+        //
+        return this.captchaObj.verify();
+      }
       axios({
-          method:'GET',
-          url:`http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+        method: "GET",
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
-         const data = res.data.data;
-         window.initGeetest({
+        const data = res.data.data;
+        window.initGeetest(
+          {
             // 以下配置参数来自服务端 SDK
             gt: data.gt,
             challenge: data.challenge,
             offline: !data.success,
             new_captcha: data.new_captcha,
-            product: 'bind' // 隐藏按钮式
-        }, (captchaObj) => {
-            this.captchaObj = captchaObj
+            product: "bind" // 隐藏按钮式
+          },
+          captchaObj => {
+            this.captchaObj = captchaObj;
             // 这里可以调用验证实例 captchaObj 的实例方法
-            captchaObj.onReady(function() {
+            captchaObj
+              .onReady(function() {
                 // 只有ready了才能显示验证码
                 captchaObj.verify();
-            }).onSuccess(function() {
+              })
+              .onSuccess(function() {
                 const {
-                 geetest_challenge: challenge,
-                 geetest_seccode: seccode,
-                 geetest_validate: validate } =
-                captchaObj.getValidate()
+                  geetest_challenge: challenge,
+                  geetest_seccode: seccode,
+                  geetest_validate: validate
+                } = captchaObj.getValidate();
                 axios({
-                   method: 'GET',
-                   url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
-                   params: {
-                      challenge,
-                      validate,
-                      seccode
+                  method: "GET",
+                  url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+                  params: {
+                    challenge,
+                    validate,
+                    seccode
                   }
                 }).then(res => {
-                    // 开启倒计时效果
-                    // const time_count = 60;
-                    // if (!this.timer) {
-                    //     this.count = time_count
-                    //     this.show = false
-                    //     this.timer = setInterval(() => {
-                    //         if (this.count > 0 && this.count <=time_count) {
-                    //             this.count--;
-                    //         } else {
-                    //             this.show = true
-                    //             clearInterval(this.timer)
-                    //             this.timer = null
-                    //         }
-                    //     } , 1000)
-                    // }
-                })
-            })
-        })
-      })
+                  // 开启倒计时效果
+                  // const time_count = 60;
+                  // if (!this.timer) {
+                  //     this.count = time_count
+                  //     this.show = false
+                  //     this.timer = setInterval(() => {
+                  //         if (this.count > 0 && this.count <=time_count) {
+                  //             this.count--;
+                  //         } else {
+                  //             this.show = true
+                  //             clearInterval(this.timer)
+                  //             this.timer = null
+                  //         }
+                  //     } , 1000)
+                  // }
+                });
+              });
+          }
+        );
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
@@ -174,7 +205,7 @@ export default {
   justify-content: center;
   align-items: center;
   //   background-color: skyblue;
-  background-image: url(2.jpg);
+  background-image: url(./2.jpg);
   background-repeat: no-repeat;
   background-position: center top;
   background-size: 100% 100%;
