1a145cf6d44c0dccbefaf000f5a92aeab39a1456
GRZ <908482128@qq.com>
2019-06-27 08:51:34 +0800

    处理文章显示状态

--- src/main.js
+++ src/main.js
@@ -5,19 +5,43 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import axios from 'axios'
+// 处理js大数字
+import JSONbig from 'json-bigint'
 
 // 优先查找文件，如果文件找不到则找目录
 // 找到目录，优先加载目录中的index.js
 import './styles/index.less'
 // 引入进度导航条
 import 'nprogress/nprogress.css'
+
+// 几乎每个组件都要使用axios 去发请求 频繁的 import 就变得非常麻烦
+//  我们可以将一些频繁使用的成员放到Vue.prototype 中 然后就可以在 组件中 this.xxx使用了
+//  为什么 ？ 因为所有组件都是Vue的实例
+// 所以往 Vue.prototype 中添加的成员可以直接通过组建实例 this进行访问
+// 往Vue原型对象中添加成员，尽量使用$名字 起名字，目的是为了防止和组件中的成员冲突
+Vue.prototype.$http = axios
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 // import Axios from 'axios';
 // 配置axios的基础路径
 // 也就是说配置了这个东西你就不用每次都写长长的 http://xxx
 // 只需要写 例如 axios({ url: '/'})
 // 路劲中的 ./ 多退少补
 axios.defaults.baseURL = 'http://toutiao.course.itcast.cn/mp/v1_0/'
-
+// 使用JSONbig 处理返回住居中超出js安全整数范围的数字
+// JSONbig自己会分析数据中的哪个数字超出整数范围了
+ //  由于后端的数据id超出了js安全正数范围 所以导致正数无法精确表示
+  // 可以使用JSON-biginit 它会帮你把超出范围的数字给处理好
+axios.defaults.transformResponse = [function(data) {
+  // JSONbig.parse类似于JSON.parse，它的作用是将JSON格式字符串转换为javascript对象
+  // 唯一的区别就是：JSONbig.parse会检测被转发数据是否超出js安全整数范围
+  // 如果data 不是一个JSON 格式字符串，会导致JSONbig.parse转换失败并异常
+  // 例如我们执行删除操作，后端会返回一个204状态码，但是没有返回任何数据，也就是空字符串
+  // 那这里JSONbig.parse(空) 空字符串 就报错了
+  // data是未经处理的后端响应数据：JSON 字符串
+    // data数据可能不是标准的JSON格式字符串，否则会导致JSONbig.parse(data)转换失败报错
+    return JSONbig.parse(data)
+}]
 /**
  *   Axios 请求拦截器 所有使用axios发起的请求都要先经过这里
  *  config 是本次请求相关的配置对象
@@ -41,7 +65,12 @@ axios.interceptors.request.use(config => {
 //  */ 统一处理响应的数据格式
 
 axios.interceptors.response.use(response => { // >= 200或者 < 400 的状态码进入这里
-  return response.data.data
+  // 如果返回的数据格式是对象‘
+  if (typeof response.data === 'object' && response.data.data) {
+    return response.data.data
+  } else {
+    return response.data
+  }
 }, error => { // >= 400 的状态码会进入这里
   const status = error.response.status
   if (status === 401) {
@@ -55,16 +84,6 @@ axios.interceptors.response.use(response => { // >= 200或者 < 400 的状态码
   }
   return Promise.reject(error);
 });
-
-// 几乎每个组件都要使用axios 去发请求 频繁的 import 就变得非常麻烦
-//  我们可以将一些频繁使用的成员放到Vue.prototype 中 然后就可以在 组件中 this.xxx使用了
-//  为什么 ？ 因为所有组件都是Vue的实例
-// 所以往 Vue.prototype 中添加的成员可以直接通过组建实例 this进行访问
-// 往Vue原型对象中添加成员，尽量使用$名字 起名字，目的是为了防止和组件中的成员冲突
-Vue.prototype.$http = axios
-Vue.use(ElementUI)
-
-Vue.config.productionTip = false
 
 new Vue({
   router,
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,141 +1,192 @@
 <template>
   <div>
     <el-card class="filter-card">
-        <div slot="header" class="clearfix">
-          <span>筛选条件</span>
-        </div>
-        <el-form ref="form" :model="form" label-width="100px">
-          <el-form-item label="文章状态：">
-            <el-radio-group v-model="form.resource">
-              <el-radio label="全部"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="频道列表">
-            <el-select v-model="form.region" placeholder="请选择">
-              <el-option label="开发者资讯" value="shanghai"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="活动时间">
-            <el-date-picker
-              v-model="form.value"
-              type="daterange"
-              range-separator="至"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期">
-            </el-date-picker>
-          </el-form-item>
-        </el-form>
+      <div slot="header" class="clearfix">
+        <span>筛选条件</span>
+      </div>
+      <el-form ref="form" :model="form" label-width="100px">
+        <el-form-item label="状态：">
+          <el-radio-group v-model="form.resource">
+            <el-radio
+            v-for="item in statTypes"
+            :key="item.label"
+            :label="item.label">
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择">
+            <el-option
+            v-for="item in channels"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-date-picker
+            v-model="form.value"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+      </el-form>
     </el-card>
-     <el-card class="filter-card">
-        <div slot="header" class="clearfix">
-          <span>共找到0条符合条件的内容</span>
-        </div>
-        <el-table
-          :data="articles"
-          v-loading="loading"
-          style="width: 100%">
-          <el-table-column
-            label="封面"
-            width="60">
-            <!-- <template slot-scope="scope"> -->
-              <!-- <img width="50" :src="scope.row.cover.images[0]"> -->
-            <!-- </template> -->
-          </el-table-column>
-          <el-table-column
-            prop="title"
-            label="标题"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="pubdate"
-            width="180"
-            label="日期">
-          </el-table-column>
-           <el-table-column
-            prop="status"
-            label="状态">
-          </el-table-column>
-          <el-table-column
-            label="状态">
-            <template slot-scope="scope">
-              <el-button plain type="success">修改</el-button>
-              <el-button plain type="success" @click="handleDelete(scope.row)">删除</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
+    <el-card class="filter-card">
+      <div slot="header" class="clearfix">
+        <span>共找到0条符合条件的内容</span>
+      </div>
+      <el-table :data="articles" v-loading="loading" style="width: 100%">
+        <el-table-column label="封面" width="60">
+          <!-- <template slot-scope="scope"> -->
+          <!-- <img width="50" :src="scope.row.cover.images[0]"> -->
+          <!-- </template> -->
+        </el-table-column>
+        <el-table-column prop="title" label="标题" width="180"></el-table-column>
+        <el-table-column prop="pubdate" width="180" label="日期"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag :type="statTypes[scope.row.status].type">{{ statTypes[scope.row.status].label }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button plain type="success">修改</el-button>
+            <el-button plain type="danger" @click="handleDelete(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
     <!-- 分页 -->
     <el-pagination
       background
       layout="prev, pager, next"
-      @current-change = "handleCurrentChages"
-      :disabled = "loading"
-      :total="totalPage">
-    </el-pagination>
+      @current-change="handleCurrentChages"
+      :disabled="loading"
+      :total="totalPage"
+    ></el-pagination>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: 'ArticlesList',
+  name: "ArticlesList",
   data() {
     return {
       articles: [],
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
         delivery: false,
         type: [],
-        resource: '',
-        desc: '',
-        value: ''
+        resource: "",
+        desc: "",
+        value: ""
       },
       totalPage: 0,
       page: 1,
-      loading: false
-    }
+      loading: false,
+      statTypes: [
+        {
+          type: "info",
+          label: "草稿"
+        },
+        {
+          type: "",
+          label: "待审核"
+        },
+        {
+          type: "success",
+          label: "审核通过"
+        },
+        {
+          type: "warning",
+          label: "审核失败"
+        },
+        {
+          type: "danger",
+          label: "已删除"
+        }
+      ],
+      channels: [] // 频道列表
+    };
   },
   created() {
-    this.loadArticles()
+    this.loadArticles();
+    this.loadChannels() // 加载频道列表
   },
   methods: {
+    // 加载文章列表
     loadArticles(page = 1) {
-      this.loading = true
+      this.loading = true;
       this.$http({
-        method: 'GET',
-        url: '/articles',
+        method: "GET",
+        url: "/articles",
         params: {
           page,
           per_page: 10
         }
         // 这里用data就表示response.data.data
       }).then(data => {
-        console.log(data)
-        this.articles = data.results
-        this.totalPage = data.total_count
-        this.loading = false
+        console.log(data);
+        this.articles = data.results;
+        this.totalPage = data.total_count;
+        this.loading = false;
+      });
+    },
+    // 加载频道列表
+    loadChannels() {
+      this.$http({
+          method: 'GET',
+          url: '/channels'
+      }).then(data => {
+          this.channels=data.channels
       })
     },
+    // 点击分页
     handleCurrentChages(page) {
       // 当点击页数的时候，将此时的page赋值给page
-      this.page = page
+      this.page = page;
       // element-ui封装的页码改变事件，当页码发生改变的时候，就发送axios请求
-      this.loadArticles(page)
+      this.loadArticles(page);
     },
     // 删除功能
     handleDelete(article) {
-      this.$http({
-        method: 'DELETE',
-        url: `/articles/${article.id}`
-      }).then(data => {
-        // console.log(data)
+      this.$confirm("确认删除吗？", "删除提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       })
+        .then(() => {
+          // 确定执行
+          // 发送删除请求
+          this.$http({
+            method: "DELETE",
+            url: `/articles/${article.id}`
+          }).then(data => {
+            // 提示删除成功
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            // 重新加载数据列表
+            this.loadArticles(this.page);
+          });
+        })
+        .catch(() => {
+          // 取消执行
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
