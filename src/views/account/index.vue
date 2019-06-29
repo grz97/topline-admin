bba755735bc4d8b23534c7ed01acc3d11a2619f3
GRZ <908482128@qq.com>
2019-06-29 21:21:34 +0800

    账户设置--头像上传完成

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -26,54 +26,125 @@
           </el-form-item>
         </el-form>
       </el-col>
-      <el-col :offset="2" :span="4"></el-col>
+      <el-col :offset="2" :span="4">
+        <!--
+          el-upload上传组件，它会自动将用户选择的图片区请求上传，我们要做的
+          就是配置一下action请求地址
+          由于它用的自己内部的请求，不是使用的axios去发请求
+          完整路径
+          它的请求也不会经过axios拦截器，所以需要手动配置token
+          可惜的是它不支持自定义请求方法，前功尽弃
+        -->
+        <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/photo"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :http-request="handleUpload"
+        >
+          <img class="avatar" v-if="userInfo.photo" :src="userInfo.photo">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-col>
     </el-row>
   </el-card>
 </template>
 
 <script>
 export default {
-  name: 'AccountSetting',
+  name: "AccountSetting",
   data() {
     return {
-      userInfo: {}
-    }
+      userInfo: {},
+      token: JSON.parse(window.localStorage.getItem("user_info")).token
+    };
   },
   created() {
-    this.loadUserInfo()
+    this.loadUserInfo();
   },
   methods: {
     loadUserInfo() {
       this.$http({
-        method: 'GET',
-        url: '/user/profile'
+        method: "GET",
+        url: "/user/profile"
       }).then(data => {
-        this.userInfo = data
-      })
+        this.userInfo = data;
+      });
     },
     handleUpdate() {
-      const { name, intro, email } = this.userInfo
+      const { name, intro, email } = this.userInfo;
       this.$http({
-        method: 'PATCH',
-        url: '/user/profile',
+        method: "PATCH",
+        url: "/user/profile",
         data: {
           name,
           intro,
           email
         }
-      }).then(() => {
-        this.$message({
-          type: 'success',
-          message: '更新用户信息成功'
-        })
-      }).catch(err => {
-        console.log(err)
-        this.$message.error('更新用户信息失败')
       })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "更新用户信息成功"
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          this.$message.error("更新用户信息失败");
+        });
+    },
+    handleAvatarSuccess() {},
+    beforeAvatarUpload() {},
+    handleUpload(uploadConfig) {
+      // axios上传文件
+      //  1 构建一个表单对象   将文件对象添加到formData中
+      //  2 将formData 配置到请求体data选项中
+      const formData = new FormData()
+      formData.append("photo", uploadConfig.file);
+      this.$http({
+        method: "PATCH",
+        url: "/user/photo",
+        data: formData
+      })
+        .then((data) => {
+          this.userInfo.photo=data.photo
+          this.$message({
+            type: "success",
+            message: "上传成功"
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          this.$meaage.error("上传失败");
+        });
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
