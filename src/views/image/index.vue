8a7cc8b43737b185493d401e04ecc56ddc36b128
GRZ <908482128@qq.com>
2019-06-30 11:51:17 +0800

    处理收藏功能&&删除图片

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card class="image-card">
     <div slot="header">
       <span>素材管理</span>
     </div>
@@ -11,19 +11,20 @@
       <el-button type="primary">上传图片</el-button>
     </div>
     <el-row :gutter="20">
-    <el-col :span="7" v-for="item in images" :key="item.id">
-      <el-card :body-style="{ padding: '0px' }">
-        <img :src="item.url" class="image" style="max-width: 100%;">
-        <!-- <div style="padding: 10px; display: flex; justify-content: center;"> -->
-        <el-button
-          type="primary"
-          :icon="item.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"
-          circle
-          plain
-        ></el-button>
-        <el-button type="primary" icon="el-icon-delete" circle plain></el-button>
-      </el-card>
-    </el-col>
+      <el-col :span="7" v-for="item in images" :key="item.id">
+        <el-card :body-style="{ padding: '0px' }">
+          <img :src="item.url" class="image" style="max-width: 100%;">
+          <!-- <div style="padding: 10px; display: flex; justify-content: center;"> -->
+          <el-button
+            type="primary"
+            :icon="item.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"
+            circle
+            plain
+            @click="handleCollect(item)"
+          ></el-button>
+          <el-button type="primary" icon="el-icon-delete" circle plain @click="handleDelete(item)"></el-button>
+        </el-card>
+      </el-col>
     </el-row>
   </el-card>
 </template>
@@ -48,10 +49,58 @@ export default {
       }).then(data => {
         this.images = data.results;
       });
+    },
+    // 收藏图片素材
+    handleCollect(item) {
+      const collect = !item.is_collected;
+      this.$http({
+        method: "PUT",
+        url: `/user/images/${item.id}`,
+        data: {
+          collect: !item.is_collected
+        }
+      })
+        .then(data => {
+          item.is_collected = collect;
+          this.$message({
+            type: "success",
+            message: `${collect ? "" : "取消"}收藏成功`
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          this.$message.error(`${collect ? "" : "取消"}收藏失败`);
+        });
+    },
+    // 删除图片素材
+    handleDelete(item) {
+      this.$http({
+        method: 'DELETE',
+        url: `/user/images/${item.id}`
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "删除成功"
+          });
+          // 删除之后刷新数据列表
+          this.loadImages()
+        })
+        .catch(err => {
+          console.log(err);
+          this.$message.error("删除失败");
+        });
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.image-card {
+  .action {
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+  }
+}
 </style>
